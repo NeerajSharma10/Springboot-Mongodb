8257bc99cd92405b7198625effac5a0c81e82ea8
NeerajSharma10 <neerajsh1012@gmail.com>
2023-12-23 23:09:51 +0530

    Committed the change

---
+++ src/test/java/com/njcoder/springbootmongodb/controller/ExpenseControllerTest.java
@@ -0,0 +1,14 @@
+package com.njcoder.springbootmongodb.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseControllerTest {
+
+    @Test
+    void checkTheTests() {
+        //Doing nothing
+    }
+
+}
